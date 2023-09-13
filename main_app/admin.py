a8af235e9120b44dc33debbb3b893b41c046315e
Timm Schoenborn <timmschoenborn@gmail.com>
2023-09-13 13:35:13 -0400

    admin can add feedings to cats

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # import our cat model
-from .models import Cat
+from .models import Cat, Feeding
 
 # Register your models here.
 admin.site.register(Cat)
+admin.site.register(Feeding)
--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,13 @@ from django.db import models
 # need the reverse method from django urls for redirects
 from django.urls import reverse
 
+# A tuple of 2-tuples
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 # Create your models here.
 class Cat(models.Model):
     name = models.CharField(max_length=100)
@@ -15,3 +22,22 @@ class Cat(models.Model):
     # this is used for redirects from class based views
     def get_absolute_url(self):
         return reverse('detail', kwargs={'cat_id': self.id})
+
+
+# Model for Feedings (Cat -|---< Feeding)
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        # add the choices field option (creates a dropdown)
+        choices=MEALS,
+        # set a default for our choices
+        default=MEALS[0][0]
+    )
+
+    # creates cat FK reference
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # this will use a method for obtaining the value of a field.choice
+        return f"{self.get_meal_display()} on {self.date}"
