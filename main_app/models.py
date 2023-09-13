fa478b51e8bbdd5dd79b86495866c2db749a81e9
Timm Schoenborn <timmschoenborn@gmail.com>
2023-09-13 14:57:51 -0400

    users are shown if cat is hungry

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 # need the reverse method from django urls for redirects
 from django.urls import reverse
+# import date module from datetime
+from datetime import date
 
 # A tuple of 2-tuples
 MEALS = (
@@ -22,6 +24,13 @@ class Cat(models.Model):
     # this is used for redirects from class based views
     def get_absolute_url(self):
         return reverse('detail', kwargs={'cat_id': self.id})
+    
+    # add a method to determine cat hunger
+    def fed_for_today(self):
+        # filter produces an <QuerySet> for all feedings from current date
+        # count the items in that array, compare to the length of MEALS (tuple)
+        # we'll return a boolean that we can use in our template
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
 
 # Model for Feedings (Cat -|---< Feeding)
@@ -41,3 +50,7 @@ class Feeding(models.Model):
     def __str__(self):
         # this will use a method for obtaining the value of a field.choice
         return f"{self.get_meal_display()} on {self.date}"
+    
+    # add Meta class to change the default sorting
+    class Meta:
+        ordering = ['-date']
